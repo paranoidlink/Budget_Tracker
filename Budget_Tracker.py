3ae7eeae685d716276ab7588e65ffe5cbd2e994d
paranoidlink <ben.atherton.1998@gmail.com>
2024-11-17 04:24:32 +0000

    Added user interaction and saving the list as a csv file

--- Budget_Tracker.py
+++ Budget_Tracker.py
@@ -1,11 +1,14 @@
-def Budget_Input(budget_list, type, description, amount, catagory):
-    while type.lower() not in ["expense", "income"]:
+import csv
+import sys
+
+def Budget_Input(budget_list, transaction_type, description, amount, category):
+    while transaction_type.lower() not in ["expense", "income"]:
         print("Please enter a valid transaction type either expense or income")
-        type = input()
+        transaction_type = input()
     
-    while catagory == None:
+    while category == None:
         print("Please enter a category for this transaction")
-        catagory = input()
+        category = input()
 
     while description == None:
         print("Please enter a valid description for this transaction")
@@ -16,10 +19,10 @@ def Budget_Input(budget_list, type, description, amount, catagory):
             return budget_list
         try:
             amount = float(amount)
-            if type == "income" and amount <= 0:
-                    print("Amount lower than 0 error \n Please enter a valid number greater than 0 or enter restart if you've made a mistake")
+            if transaction_type == "income" and amount <= 0:
+                    print("Lower than 0 error \n Please enter a valid number greater than 0 or enter restart if you've made a mistake")
                     amount = input()
-            elif type == "expense" and amount >= 0:
+            elif transaction_type == "expense" and amount >= 0:
                 print("Greater than 0 error \n Please enter a valid negative number for this transaction or enter restart if you've made a mistake")
                 amount = input()
             else:
@@ -28,13 +31,13 @@ def Budget_Input(budget_list, type, description, amount, catagory):
             print("amount entered is not a number please enter a valid number or enter restart if you've made a mistake")
             amount = input()
 
-    budget_entry = [type, description, amount, catagory]
+    budget_entry = [transaction_type, description, amount, category]
     budget_list.append(budget_entry)
     return budget_list
 
 def Get_Input():
-    print("Please enter what type of transaction you'd like to perform either \'expense\' or  \'income\'")
-    type = input()
+    print("Please enter what type of transaction you'd like to perform either \'expense\' or  \'income\' or enter \'exit\' to quit the")
+    transaction_type = input()
     print("Please enter a description for this transaction")
     description = input()
     print("Please enter an amount for this transaction")
@@ -44,6 +47,28 @@ def Get_Input():
             break
         except ValueError:
             print("Please enter a valid number")
-    print("Please enter a catagory for this transaction")
-    catagory = input()
-    return type,description,amount,catagory
+    print("Please enter a category for this transaction")
+    category = input()
+    return transaction_type,description,amount,category
+
+def Save_File(budget_list):
+    with open("budget.csv", "a", newline='') as file:
+        writer = csv.writer(file)
+
+        if file.tell() == 0:
+            writer.writerow(["Type", "Description", "Amount", "Category"])
+
+        writer.writerows(budget_list)
+
+budget_list = []
+print("Hello welcome to the budget tracker enter \'start\' to begin or \'exit\' to exit the program")
+while True:
+    responce = input()
+    if responce.lower() == "exit":
+        Save_File(budget_list)
+        sys.exit("Data has been saved to budget.csv now closing the program")
+    elif responce.lower() == "start":
+        transaction_type,description,amount,category = Get_Input()
+        Budget_Input(budget_list, transaction_type, description, amount, category)
+    print("Your updated budget list is " + str(budget_list) + "\nif you would like to add more enter \'start\' again if you'd like to exit enter \'exit\'")
+
