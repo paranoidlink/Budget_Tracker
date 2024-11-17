e9f657690128d4daac7d2d092720f7d6583b70f0
paranoidlink <ben.atherton.1998@gmail.com>
2024-11-17 05:26:28 +0000

    lots of refactoring to make the code more modular and efficent

--- Budget_Tracker.py
+++ Budget_Tracker.py
@@ -1,19 +1,27 @@
 import csv
 import sys
+def Save_File(budget_list):
+    with open("budget.csv", "a", newline='') as file:
+        writer = csv.writer(file)
 
-def Budget_Input(budget_list, transaction_type, description, amount, category):
+        if file.tell() == 0:
+            writer.writerow(["Type", "Description", "Amount", "Category"])
+
+        writer.writerows(budget_list)
+
+def Validation_Helper(criteria):
+    while criteria == "":
+        print("Please enter a valid " + criteria + " for this transaction")
+        criteria = input()
+        return criteria
+    
+def Type_Helper(transaction_type):
     while transaction_type.lower() not in ["expense", "income"]:
         print("Please enter a valid transaction type either expense or income")
         transaction_type = input()
-    
-    while category == None:
-        print("Please enter a category for this transaction")
-        category = input()
+        return transaction_type
 
-    while description == None:
-        print("Please enter a valid description for this transaction")
-        description = input()
-
+def Amount_Helper(amount, transaction_type):
     while True:
         if isinstance(amount, str) and amount.lower() == "restart":
             return budget_list
@@ -26,13 +34,22 @@ def Budget_Input(budget_list, transaction_type, description, amount, category):
                 print("Greater than 0 error \n Please enter a valid negative number for this transaction or enter restart if you've made a mistake")
                 amount = input()
             else:
-                break
+                return amount
         except ValueError:
             print("amount entered is not a number please enter a valid number or enter restart if you've made a mistake")
             amount = input()
 
+
+def Budget_Input(budget_list, transaction_type, description, amount, category):
+
+    transaction_type = Type_Helper(transaction_type)
+    category = Validation_Helper(category)
+    description = Validation_Helper(description)
+    amount = Amount_Helper(amount, transaction_type)
+
     budget_entry = [transaction_type, description, amount, category]
     budget_list.append(budget_entry)
+    Save_File([budget_entry])
     return budget_list
 
 def Get_Input():
@@ -51,24 +68,28 @@ def Get_Input():
     category = input()
     return transaction_type,description,amount,category
 
-def Save_File(budget_list):
-    with open("budget.csv", "a", newline='') as file:
-        writer = csv.writer(file)
-
-        if file.tell() == 0:
-            writer.writerow(["Type", "Description", "Amount", "Category"])
-
-        writer.writerows(budget_list)
+def balance_checker(budget_list):
+    result = 0
+    if budget_list == []:
+        print("Please add some items to todays list before checking the balance")
+        return 0
+    for item in budget_list:
+        result += item[2]
+    return result
 
 budget_list = []
 print("Hello welcome to the budget tracker enter \'start\' to begin or \'exit\' to exit the program")
 while True:
-    responce = input()
-    if responce.lower() == "exit":
-        Save_File(budget_list)
-        sys.exit("Data has been saved to budget.csv now closing the program")
-    elif responce.lower() == "start":
+    response = input()
+    if response.lower() == "exit":
+        sys.exit("Thank you for using the budget tracker your data has been saved in the same folder as this program")
+    elif response.lower() == "balance":
+        balance = balance_checker(budget_list)
+        print(f"Todays balance is £{balance:.2f}")
+    elif response.lower() == "start":
         transaction_type,description,amount,category = Get_Input()
         Budget_Input(budget_list, transaction_type, description, amount, category)
-    print("Your updated budget list is " + str(budget_list) + "\nif you would like to add more enter \'start\' again if you'd like to exit enter \'exit\'")
+    else:
+        print("Please enter a valid option of either \'start\', \'budget\' or \'exit\'")
+    print("Your updated budget list is " + str(budget_list) + "\nif you would like to add more enter \'start\' again \nIf you'd like to check your balance for todays inputs enter \'balance\'\nIf you'd like to exit enter \'exit\'")
 
